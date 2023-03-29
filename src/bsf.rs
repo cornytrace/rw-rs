d7dc32b059fc410dd06e43bd397c2d56cc6acdb3
Cornee Traas <corneetraas@hotmail.com>
2023-03-29 17:00:02 +0200

    Port BSF to nom

--- src/bsf.rs
+++ src/bsf.rs
@@ -1,59 +1,79 @@
-use std::io::Cursor;
-use std::mem::size_of;
-
-use binrw::binread;
-use binrw::until_eof;
-use binrw::BinRead;
+use nom::bytes::complete::take;
+use nom::multi::many0;
+use nom::number::complete::le_u32;
+use nom::IResult;
 use num_derive::FromPrimitive;
 use num_traits::FromPrimitive;
 
-#[derive(FromPrimitive)]
+#[derive(FromPrimitive, Debug)]
 #[repr(u32)]
 pub enum ChunkType {
     Struct = 0x00000001,
+    String = 0x00000002,
+    Extension = 0x00000003,
+    Camera = 0x00000005,
+    Texture = 0x00000006,
+    Material = 0x00000007,
+    MaterialList = 0x00000008,
+    FrameList = 0x0000000E,
     Clump = 0x00000010,
+    Frame = 0x0253F2FE,
+}
+impl ChunkType {
+    fn has_children(&self) -> bool {
+        match self {
+            ChunkType::Struct => false,
+            ChunkType::String => false,
+            ChunkType::Frame => false,
+            _ => true,
+        }
+    }
 }
 
-#[derive(BinRead)]
-#[brw(little)]
-pub struct Bsf {
-    #[br(parse_with = until_eof)]
-    chunks: Vec<BsfChunk>,
-}
-
-#[derive(BinRead)]
-#[brw(little)]
+#[derive(Debug)]
 pub struct BsfChunk {
-    pub ty: u32,
+    pub ty: ChunkType,
     pub size: u32,
     pub version: u32,
-    #[br(count = size)]
-    data: Vec<u8>,
-}
-impl BsfChunk {
-    pub fn get_children(&self) -> Vec<BsfChunk> {
-        let mut res = Vec::new();
-        if has_children(self.ty) {
-            let mut cursor = Cursor::new(&self.data);
-            while cursor.position() < self.size.into() {
-                res.push(BsfChunk::read(&mut cursor).unwrap())
-            }
-        }
-        res
-    }
+    pub content: BsfChunkContent,
 }
 
-fn has_children(ty: u32) -> bool {
-    match ChunkType::from_u32(ty) {
-        Some(ChunkType::Struct) => false,
-        Some(ChunkType::Clump) => true,
-        _ => unimplemented!(),
+pub fn parse_bsf_chunk(i: &[u8]) -> IResult<&[u8], BsfChunk> {
+    let (i, ty) = le_u32(i)?;
+    let ty = ChunkType::from_u32(ty).unwrap_or_else(|| unimplemented!("0x{:08X}", ty));
+    let (i, size) = le_u32(i)?;
+    let (i, version) = le_u32(i)?;
+    let (i, data) = take(size)(i)?;
+    let content;
+    let mut i = i;
+    if ty.has_children() {
+        let (i2, res) = many0(parse_bsf_chunk)(data)?;
+        i = i2;
+        content = BsfChunkContent::Children(res);
+    } else {
+        content = BsfChunkContent::Data(data.to_vec());
     }
+
+    Ok((
+        i,
+        BsfChunk {
+            ty,
+            size,
+            version,
+            content,
+        },
+    ))
+}
+
+#[derive(Debug)]
+pub enum BsfChunkContent {
+    Data(Vec<u8>),
+    Children(Vec<BsfChunk>),
 }
 
 #[cfg(test)]
 mod tests {
-    use std::fs::File;
+    use std::fs;
 
     use anyhow::Result;
 
@@ -61,8 +81,9 @@ mod tests {
 
     #[test]
     fn it_works() -> Result<()> {
-        let mut file = File::open("player.dff")?;
-        let _dff = Bsf::read(&mut file)?;
+        let file = fs::read("player.dff")?;
+        let (_, dff) = parse_bsf_chunk(&file).unwrap();
+        dbg!(dff);
         Ok(())
     }
 }
