53a27766a60f8fa44c5c44cbf1406342bb726062
Cornee Traas <corneetraas@hotmail.com>
2023-07-29 11:39:35 +0200

    Implement Geometry parsing Probably not giving the right data yet

--- src/bsf.rs
+++ src/bsf.rs
@@ -1,7 +1,8 @@
 use nom::bytes::complete::take;
-use nom::multi::many0;
+use nom::multi::{count, many0};
 use nom::number::complete::le_u32;
 use nom::IResult;
+use nom_derive::*;
 use num_derive::FromPrimitive;
 use num_traits::FromPrimitive;
 
@@ -43,6 +44,15 @@ fn parse_chunk_content<'a>(
     i: &'a [u8],
 ) -> IResult<&'a [u8], BsfChunkContent> {
     match ty {
+        ChunkType::String => take(size)(i).map(|(i, data)| {
+            (
+                i,
+                BsfChunkContent::String(std::str::from_utf8(data).unwrap().to_owned()),
+            )
+        }),
+        ChunkType::Geometry => {
+            RpGeometry::parse(i).map(|(i, geometry)| (i, BsfChunkContent::RpGeometry(geometry)))
+        }
         _ => take(size)(i).map(|(i, data)| (i, BsfChunkContent::Data(data.to_vec()))),
     }
 }
@@ -66,6 +76,9 @@ pub fn parse_bsf_chunk(i: &[u8]) -> IResult<&[u8], BsfChunk> {
     let mut content = BsfChunkContent::None;
     if ty.has_children() {
         (_, children) = many0(parse_bsf_chunk)(data)?;
+        if !children.is_empty() && children[0].ty == ChunkType::Struct {
+            (_, content) = parse_chunk_content(&ty, children[0].size, &data[3 * 4..])?;
+        }
     } else {
         (_, content) = parse_chunk_content(&ty, size, data)?;
     }
@@ -87,35 +100,142 @@ pub enum BsfChunkContent {
     None,
     Data(Vec<u8>),
     String(String),
+    RpGeometry(RpGeometry),
 }
 
-struct RwRGBA {
-    r: u8,
-    g: u8,
-    b: u8,
-    a: u8,
+#[derive(Clone, Copy, Debug, Nom)]
+pub struct RwRGBA {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
 }
 
-struct RwTexCoords {
-    u: f32,
-    v: f32,
+#[derive(Clone, Copy, Debug, Nom)]
+pub struct RwTexCoords {
+    pub u: f32,
+    pub v: f32,
 }
 
-struct RpTriangle {
-    vertex2: u16,
-    vertex1: u16,
-    material_id: u16,
-    vertex3: u16,
+#[derive(Clone, Copy, Debug, Nom)]
+pub struct RpTriangle {
+    pub vertex2: u16,
+    pub vertex1: u16,
+    pub material_id: u16,
+    pub vertex3: u16,
 }
 
-struct RpGeometry {
+impl RpTriangle {
+    pub fn as_arr(self) -> [u16; 3] {
+        [self.vertex1, self.vertex2, self.vertex3]
+    }
+}
+
+#[derive(Clone, Debug, Nom)]
+pub struct RwV3d {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+impl RwV3d {
+    pub fn as_arr(&self) -> [f32; 3] {
+        [self.x, self.y, self.z]
+    }
+}
+
+#[derive(Clone, Debug, Nom)]
+pub struct RwSphere {
+    pub pos: RwV3d,
+    pub radius: f32,
+}
+
+#[derive(Clone, Debug)]
+pub struct RpGeometry {
     format: u32,
-    num_triangles: u32,
-    num_vertices: u32,
-    num_morph: u32,
-    prelit: Vec<RwRGBA>,
-    tex_coords: Vec<RwTexCoords>,
-    triangles: Vec<RpTriangle>,
+    pub num_triangles: u32,
+    pub num_vertices: u32,
+    pub num_morphs: u32,
+    pub prelit: Vec<RwRGBA>,
+    pub tex_coords: Vec<Vec<RwTexCoords>>,
+    pub triangles: Vec<RpTriangle>,
+    pub vertices: Vec<RwV3d>,
+    pub normals: Vec<RwV3d>,
+}
+
+const RP_GEOMETRYTRISTRIP: u32 = 0x00000001;
+const RP_GEOMETRYTEXTURED: u32 = 0x00000004;
+const RP_GEOMETRYPRELIT: u32 = 0x00000008;
+const RP_GEOMETRYTEXTURED2: u32 = 0x00000080;
+const RP_GEOMETRYNATIVE: u32 = 0x01000000;
+
+impl RpGeometry {
+    fn parse(i: &[u8]) -> IResult<&[u8], Self> {
+        let (i, format) = le_u32(i)?;
+        let (i, num_triangles) = le_u32(i)?;
+        let (i, num_vertices) = le_u32(i)?;
+        let (mut i, num_morphs) = le_u32(i)?;
+
+        let mut num_tex_sets = (format & 0x00FF0000) << 16;
+        if num_tex_sets == 0 {
+            if format & RP_GEOMETRYTEXTURED != 0 {
+                num_tex_sets = 1;
+            }
+            if format & RP_GEOMETRYTEXTURED2 != 0 {
+                num_tex_sets = 2;
+            }
+        }
+
+        let mut prelit = Vec::new();
+        let mut tex_coords = Vec::new();
+        let mut triangles = Vec::new();
+
+        if format & RP_GEOMETRYNATIVE == 0 {
+            if format & RP_GEOMETRYPRELIT != 0 {
+                (i, prelit) = count(RwRGBA::parse_le, num_vertices as usize)(i)?;
+            }
+            (i, tex_coords) = count(
+                count(RwTexCoords::parse_le, num_vertices as usize),
+                num_tex_sets as usize,
+            )(i)?;
+            (i, triangles) = count(RpTriangle::parse_le, num_triangles as usize)(i)?;
+        }
+
+        // TODO: Multiple Morph sets
+
+        let (i, _) = RwSphere::parse_le(i)?;
+        let (i, has_vertices) = le_u32(i)?;
+        let (mut i, has_normals) = le_u32(i)?;
+
+        let mut vertices = Vec::new();
+        if has_vertices > 0 {
+            (i, vertices) = count(RwV3d::parse_le, num_vertices as usize)(i)?;
+        }
+
+        let mut normals = Vec::new();
+        if has_normals > 0 {
+            (i, normals) = count(RwV3d::parse_le, num_vertices as usize)(i)?;
+        }
+
+        Ok((
+            i,
+            Self {
+                format,
+                num_triangles,
+                num_vertices,
+                num_morphs,
+                prelit,
+                tex_coords,
+                triangles,
+                vertices,
+                normals,
+            },
+        ))
+    }
+
+    pub fn is_tristrip(&self) -> bool {
+        self.format & RP_GEOMETRYTRISTRIP > 0
+    }
 }
 
 #[cfg(test)]
