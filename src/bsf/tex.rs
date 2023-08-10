53e7b989186d7dc9942631eb70dd968ce8b473c7
Cornee Traas <corneetraas@hotmail.com>
2023-08-10 15:17:13 +0200

    Add RpTexture parsing

--- src/bsf/tex.rs
+++ src/bsf/tex.rs
@@ -1,5 +1,10 @@
-use nom::{number::complete::le_u32, IResult};
+use nom::{
+    number::complete::{le_u16, le_u32, le_u8},
+    IResult, Parser,
+};
 use nom_derive::{Nom, Parse};
+use num_derive::FromPrimitive;
+use num_traits::cast::FromPrimitive;
 
 #[derive(Clone, Copy, Debug, Nom)]
 pub struct RwRGBA {
@@ -52,6 +57,55 @@ impl RpMaterial {
             Self {
                 color,
                 surface_prop,
+            },
+        ))
+    }
+}
+
+#[derive(Clone, Copy, Debug, Nom)]
+#[repr(u8)]
+pub enum TextureFilteringMode {
+    FILTERNAFILTERMODE,     // filtering is disabled
+    FILTERNEAREST,          // Point sampled
+    FILTERLINEAR,           // Bilinear
+    FILTERMIPNEAREST,       // Point sampled per pixel mip map
+    FILTERMIPLINEAR,        // Bilinear per pixel mipmap
+    FILTERLINEARMIPNEAREST, // MipMap interp point sampled
+    FILTERLINEARMIPLINEAR,  // Trilinear
+}
+
+#[derive(Clone, Copy, Debug, FromPrimitive)]
+#[repr(u8)]
+pub enum TextureAddressingMode {
+    TEXTUREADDRESSNATEXTUREADDRESS, // no tiling
+    TEXTUREADDRESSWRAP,             // tile in U or V direction
+    TEXTUREADDRESSMIRROR,           // mirror in U or V direction
+    TEXTUREADDRESSCLAMP,
+    TEXTUREADDRESSBORDER,
+}
+
+pub struct RpTexture {
+    pub filtering: TextureFilteringMode,
+    pub addressing: [TextureAddressingMode; 2],
+    pub has_mip: bool,
+}
+
+impl RpTexture {
+    pub fn parse<'a>(&mut self, i: &'a [u8]) -> IResult<&'a [u8], Self> {
+        let (i, filtering) = TextureFilteringMode::parse_le(i)?;
+        let (i, addr) = le_u8(i)?;
+        let addr_h = TextureAddressingMode::from_u8((addr & 0b11110000) >> 4).unwrap();
+        let addr_l = TextureAddressingMode::from_u8(addr & 0b00001111).unwrap();
+        let addressing = [addr_h, addr_l];
+        let (i, has_mip) = le_u16(i)?;
+        let has_mip = has_mip != 0;
+
+        Ok((
+            i,
+            Self {
+                filtering,
+                addressing,
+                has_mip,
             },
         ))
     }
